a94c7b50736abb48694d624a0d81e919b9040139
Matthias Molitor <mmo@webfactory.de>
2015-09-26 23:31:34 +0200

    added class stub

---
+++ src/Kit.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Matthimatiker\RootKit;
+
+/**
+ *
+ */
+class Kit
+{
+    public static function getProjectRoot()
+    {
+
+    }
+
+    public static function getVendorDirectory()
+    {
+
+    }
+}
