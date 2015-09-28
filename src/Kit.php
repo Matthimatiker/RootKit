105751cf4d71c4370234b3b5495fd9daff514f1c
Matthias Molitor <mmo@webfactory.de>
2015-09-28 19:47:02 +0200

    documented signatures; added basic description

--- src/Kit.php
+++ src/Kit.php
@@ -3,15 +3,26 @@
 namespace Matthimatiker\RootKit;
 
 /**
- *
+ * Provides methods to determine the path to the project root and the vendor directory
+ * in Composer enabled projects.
  */
 class Kit
 {
+    /**
+     * Returns the path to the root directory of the project.
+     *
+     * @return string
+     */
     public static function getProjectRoot()
     {
 
     }
 
+    /**
+     * Returns the path to the vendor directory.
+     *
+     * @return string
+     */
     public static function getVendorDirectory()
     {
 
